c8f5b517a8ee06e3656f334d9a6b1efa7ff3b9ee
JakubBarczynski <barczynskikuba15@gmail.com>
2026-07-10 14:11:47 +0100

    Simplify App file

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -50,47 +50,54 @@
 
 
 
+// import { useState, useEffect } from 'react';
+// import { Routes, Route, Navigate } from "react-router-dom";
+// import Products from './pages/Products';
+// import Cart from './pages/Cart';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
+// import { CartProvider } from "./context/CartContext";
+// import ComingSoon from './pages/ComingSoon';
+
+// const COMING_SOON = true; // flip to false when ready to launch
+
+// function App() {
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+//   const [authOpen, setAuthOpen] = useState(false);
+
+//   useEffect(() => {
+//     const token = localStorage.getItem("token");
+//     setIsLoggedIn(!!token);
+//   }, []);
+
+//   if (COMING_SOON) {
+//     return <ComingSoon />;
+//   }
+
+//   return (
+//     <CartProvider>
+//       <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} authOpen={authOpen} setAuthOpen={setAuthOpen} />
+//       <Routes>
+//         <Route path="/" element={<Home setIsLoggedIn={setIsLoggedIn} />} />
+//         <Route path="/products" element={<Products setIsLoggedIn={setIsLoggedIn} />} />
+//         <Route path="/cart" element={<Cart />} />
+//         <Route path="/about" element={<About />} />
+//       </Routes>
+//       <Footer />
+//     </CartProvider>
+//   );
+// }
+
+// export default App;
 
 
 
-import { useState, useEffect } from 'react';
-import { Routes, Route, Navigate } from "react-router-dom";
-import Products from './pages/Products';
-import Cart from './pages/Cart';
-import Home from './pages/Home';
-import About from './pages/About';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import { CartProvider } from "./context/CartContext";
 import ComingSoon from './pages/ComingSoon';
 
-const COMING_SOON = true; // flip to false when ready to launch
-
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [authOpen, setAuthOpen] = useState(false);
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    setIsLoggedIn(!!token);
-  }, []);
-
-  if (COMING_SOON) {
-    return <ComingSoon />;
-  }
-
-  return (
-    <CartProvider>
-      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} authOpen={authOpen} setAuthOpen={setAuthOpen} />
-      <Routes>
-        <Route path="/" element={<Home setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/products" element={<Products setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-      <Footer />
-    </CartProvider>
-  );
+  return <ComingSoon />;
 }
 
 export default App;
